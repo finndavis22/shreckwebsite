0b43b24f933956d13b7a312d5d878213edfc1a01
finn davis <finntdavis@gmail.com>
2022-08-09 11:08:45 -0400

    made shreck move button

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
+function moveShrek() {
+    console.log("button was clicked")
+    shrek = document.getElementById("shrek")
+    shrek.style.left = `${(Math.floor(Math.random()*(window.innerWidth)))-shrek.clientWidth}px`
+}
+
 
 
